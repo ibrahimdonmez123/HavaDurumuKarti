8f9e94c8a29831092570888e6b2f5c0dcc9b5306
DESKTOP-JI1JKUA\ibrahim <donmez2k@gmail.com>
2024-01-30 22:48:42 +0300

    Add project files.

---
+++ HavaDurumuKarti/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace HavaDurumuKarti
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label2.Text = DateTime.Now.ToShortDateString();
+            string api = "6b719d0cbd224a423049ac17807f36a4";
+            string connection = "http://api.openweathermap.org/data/2.5/weather?q=istanbul&mode=xml&lang=tr&units=metric&appid=" + api;
+            XDocument hava = XDocument.Load(connection);
+            var sicaklik = hava.Descendants("temperature").ElementAt(0).Attribute("value").Value;
+            label3.Text= sicaklik.ToString();
+        }
+    }
+}
